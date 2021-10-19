844f57df1422e7fa8a25a48beadc7f32b71c716a
Christian Blank <christian.blank@bonprix.net>
2021-10-19 18:20:23 +0200

    Save measure with rest on oracle db

--- bin/FetchKostalData.go
+++ bin/FetchKostalData.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
 	"time"
 
 	"example.com/kostalinverter/kostalinverter"
@@ -9,14 +11,23 @@ import (
 
 func main() {
 	var j = 0
+	var urlstr = "https://h4de06bp7uxfolh-db202110152122.adb.eu-frankfurt-1.oraclecloudapps.com/ords/powermonitor/kostal_inverter/"
+
 	for {
 		jDate, err := kostalinverter.FetchKostalDates()
 		if err != nil {
 			fmt.Println(err)
+		} else {
+			j++
+			fmt.Printf("%d : %s\n", j, jDate)
+			resp, err := http.Post(urlstr, "application/json", bytes.NewBuffer([]byte(jDate)))
+			//fmt.Println(err)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Printf("%d : %s\n", j, resp.Status)
 		}
 
-		j++
-		fmt.Printf("%d : %s\n", j, jDate)
-		time.Sleep(1 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 }
