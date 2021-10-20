d3d5be91337986b9cee9d8fd9a9209948673bf4a
Christian Blank <christian.blank@bonprix.net>
2021-10-20 10:55:09 +0200

    socket: too many open files

--- bin/FetchKostalData.go
+++ bin/FetchKostalData.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	var j = 0
-
+	var sleepTime = time.Duration(5)
 	for {
 		/*
 			type MeasureDate struct {
@@ -36,21 +36,25 @@ func main() {
 			}
 
 			fmt.Printf("%d : %s\n", j, jstring)
+			if mDate.Aktuell > 0 {
+				//Save the jstring over restfull service in the table kostal_inverter_rest
+				var urlstr = "https://h4de06bp7uxfolh-db202110152122.adb.eu-frankfurt-1.oraclecloudapps.com/ords/powermonitor/kostal_inverter/"
+				resp, err := http.Post(urlstr, "application/json", bytes.NewBuffer([]byte(jstring)))
+				if err != nil {
+					fmt.Println(err)
+				}
+				//defer resp.Body.Close()
 
-			//Save the jstring over restfull service in the table kostal_inverter_rest
-			var urlstr = "https://h4de06bp7uxfolh-db202110152122.adb.eu-frankfurt-1.oraclecloudapps.com/ords/powermonitor/kostal_inverter/"
-			resp, err := http.Post(urlstr, "application/json", bytes.NewBuffer([]byte(jstring)))
-			if err != nil {
-				fmt.Println(err)
+				fmt.Printf("%d : %s\n", j, resp.Status)
 			}
-
-			fmt.Printf("%d : %s\n", j, resp.Status)
 		}
 
 		if mDate.Aktuell == 0 {
-			time.Sleep(60 * time.Second)
+			sleepTime = time.Duration(60)
+			time.Sleep(sleepTime * time.Second)
 		} else {
-			time.Sleep(5 * time.Second)
+			sleepTime = time.Duration(5)
+			time.Sleep(sleepTime * time.Second)
 		}
 	}
 }
