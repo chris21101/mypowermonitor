0c4381b62fd0f229b2c55e72419d80eb503a5740
Christian Blank <christian.blank@bonprix.net>
2021-10-23 21:47:53 +0200

    Inverter_Rest with Oauth

--- bin/FetchKostalData.go
+++ bin/FetchKostalData.go
@@ -4,14 +4,36 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	"example.com/kostalinverter/kostalinverter"
+	"example.com/kostalinverter/oracleRestClient"
 	"example.com/kostalinverter/power_util"
 )
 
 func main() {
+	newTokenRequest := oracleRestClient.OracleTokenRequest{
+		Aouthurl:     "https://h4de06bp7uxfolh-db202110152122.adb.eu-frankfurt-1.oraclecloudapps.com/ords/pm/oauth/token",
+		ClientID:     "HOG9HK859p3I4Uk5IKbF4Q..",
+		ClientSecret: "iNCuRgR0qmd-0poma_O2ew.."}
+
+	newOracleRequest := oracleRestClient.OraclePostRequest{
+		AccessUrl:  "https://h4de06bp7uxfolh-db202110152122.adb.eu-frankfurt-1.oraclecloudapps.com/ords/pm/rest-v1/inverter/",
+		Oauthtoken: "",
+	}
+
+	if newOracleRequest.Oauthtoken == "" {
+		newtoken, err := oracleRestClient.GetOracleDBtoken(newTokenRequest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		newOracleRequest.Oauthtoken = newtoken
+	}
+
+	fmt.Printf("%s - %s\n", power_util.GetTimeStr(), newOracleRequest.Oauthtoken)
+
 	var j = 0
 	//var counter0 int = 0
 
@@ -19,43 +41,54 @@ func main() {
 		mDate, err := kostalinverter.FetchKostalValue()
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("%s - %s\n", power_util.GetTimeStr(), err)
 		} else {
 			j++
 			jstring, err := json.Marshal(mDate)
 
 			if err != nil {
-				println(err)
+				fmt.Printf("%s - %s\n", power_util.GetTimeStr(), err)
 			}
 
 			fmt.Printf("%s - %d : %s\n", power_util.GetTimeStr(), j, jstring)
-			if mDate.Aktuell > 0 {
-				//Save the jstring over restfull service in the table inverter_rest
-				var urlstr = "https://h4de06bp7uxfolh-db202110152122.adb.eu-frankfurt-1.oraclecloudapps.com/ords/pm/rest-v1/inverter/"
-				client := &http.Client{
-					Timeout: 10 * time.Second,
-				}
 
-				resp, err := client.Post(urlstr, "application/json", bytes.NewBuffer([]byte(jstring)))
+			//Save the jstring over restfull service in the table inverter_rest
+			if newOracleRequest.Oauthtoken == "" {
+				newtoken, err := oracleRestClient.GetOracleDBtoken(newTokenRequest)
 				if err != nil {
-					fmt.Println(err)
+					log.Fatal(err)
 				}
-				fmt.Printf("%s - %d : %s\n", power_util.GetTimeStr(), j, resp.Status)
-			} else {
-				//Save the jstring over restfull service in the table inverter_rest
-				var urlstr = "https://h4de06bp7uxfolh-db202110152122.adb.eu-frankfurt-1.oraclecloudapps.com/ords/pm/rest-v1/inverter/"
-				client := &http.Client{
-					Timeout: 10 * time.Second,
-				}
-
-				resp, err := client.Post(urlstr, "application/json", bytes.NewBuffer([]byte(jstring)))
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Printf("%s - %d : %s\n", power_util.GetTimeStr(), j, resp.Status)
-
-				fmt.Printf("%s - %d : %s\n", power_util.GetTimeStr(), j, "Not saved because actual_energy = 0 and daily_energie > 0")
+				newOracleRequest.Oauthtoken = newtoken
 			}
+
+			client := &http.Client{
+				Timeout: 10 * time.Second,
+			}
+
+			req, err := http.NewRequest(http.MethodPost, newOracleRequest.AccessUrl, bytes.NewBuffer([]byte(jstring)))
+			if err != nil {
+				fmt.Printf("%s - %s\n", power_util.GetTimeStr(), err)
+			}
+			req.Close = true
+			req.Header.Set("Authorization", newOracleRequest.Oauthtoken)
+
+			resp, err := client.Do(req)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Printf("%s - %d : %s\n", power_util.GetTimeStr(), j, resp.Status)
+
+			if resp.StatusCode == 401 {
+				newtoken, err := oracleRestClient.GetOracleDBtoken(newTokenRequest)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("%s - %s\n", power_util.GetTimeStr(), newtoken)
+				newOracleRequest.Oauthtoken = newtoken
+				fmt.Printf("%s - %s\n", power_util.GetTimeStr(), newOracleRequest.Oauthtoken)
+			}
+
 		}
 
 		if mDate.Aktuell == 0 {
