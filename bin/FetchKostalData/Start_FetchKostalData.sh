31530d65937faa57d86ef70c5df35dc856833cda
Christian Blank <christian@familie-blank.de>
2022-04-28 13:40:10 +0200

    Dos2Unix

---
+++ bin/FetchKostalData/Start_FetchKostalData.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export ClientName=kostal_ws
+
+./FetchKostalData
---
+++ bin/TestDiscovergyAPI/Start_TestDiscovergyAPI.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ClientName=discovergy_ws
+export DiscovergyPasswd=222Pas@Viv222
+export DiscovergyEmail=christian@familie-blank.de
+
+./TestDiscovergyAPI
